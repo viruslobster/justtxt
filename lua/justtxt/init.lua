a94d4ee0f2adc498494cccbff46704761c4c811a
Michael Stevens <msteven9@fbcom>
2022-08-27 20:14:48 -0400

    stream close bugfix, sigint, block boundries

--- lua/justtxt/init.lua
+++ lua/justtxt/init.lua
@@ -5,6 +5,22 @@ local EXE_BLOCK_END = "^#/>"
 local OUT_BLOCK_START = "--------[out]---"
 local OUT_BLOCK_END = "--------[end]---"
 
+function get_line(self, i)
+    return vim.api.nvim_buf_get_lines(self.id, i, i+1, true)[1]
+end
+
+function get_lines(self, i, j)
+    return vim.api.nvim_buf_get_lines(self.id, i, j, true)
+end
+
+function set_lines(self, i, j, lines)
+    vim.api.nvim_buf_set_lines(self.id, i, j, true, lines)
+end
+
+function append_line(self, i, line)
+    vim.api.nvim_buf_set_lines(self.id, i, i, true, {line})
+end 
+
 function buffer_data()
     local id = vim.api.nvim_get_current_buf()
     local cursor = vim.api.nvim_win_get_cursor(0)
@@ -13,34 +29,36 @@ function buffer_data()
         len = vim.api.nvim_buf_line_count(id),
         cursor_y = cursor[1] - 1,
         cursor_x = cursor[2],
+        get_line = get_line,
+        get_lines = get_lines,
+        set_lines = set_lines,
+        append_line = append_line,
     }
 end
 
 function find_run_block(buf)
     -- search upwards for start of block
-    local row = buf.cursor_y
-    local block_start = row
-    for i = row, 0, -1 do
-        local line = vim.api.nvim_buf_get_lines(buf.id, i, i+1, true)[1]
+    local block_start = buf.cursor_y
+    for i = buf.cursor_y, 0, -1 do
+        local line = buf:get_line(i)
         if line:match(EXE_BLOCK_START) then
             block_start = i
             break
         end
-        if line:match(EXE_BLOCK_END) then
+        if line:match(EXE_BLOCK_END) and i ~= buf.cursor_y then
             break -- we've gone too far
         end
     end
     
     -- search downwards for end of block
-    row = buf.cursor_y
-    local block_end = row
-    for i = row, buf.len - 1 do
-        local line = vim.api.nvim_buf_get_lines(buf.id, i, i+1, true)[1]
+    local block_end = buf.cursor_y
+    for i = buf.cursor_y, buf.len - 1 do
+        local line = buf:get_line(i)
         if line:match(EXE_BLOCK_END) then
             block_end = i
             break
         end
-        if line:match(EXE_BLOCK_START) then
+        if line:match(EXE_BLOCK_START) and i ~= buf.cursor_y then
             break -- we've gone too far
         end
     end
@@ -54,13 +72,13 @@ function get_out_block(buf, block_start, block_end)
         return nil, nil
     end
 
-    local first_line = vim.api.nvim_buf_get_lines(buf.id, start, start+1, true)[1]
+    local first_line = buf:get_line(start)
     if first_line ~= OUT_BLOCK_START then
         return nil, nil
     end
 
     for finish = start+1, buf.len do
-        local line = vim.api.nvim_buf_get_lines(buf.id, finish, finish+1, true)[1]
+        local line = buf:get_line(finish)
         if line == OUT_BLOCK_END then
             return start, finish
         end
@@ -69,13 +87,13 @@ function get_out_block(buf, block_start, block_end)
 end
 
 function create_cmd(buf, block_start, block_end)
-    local lines = vim.api.nvim_buf_get_lines(buf.id, block_start, block_end+1, true)
+    local lines = buf:get_lines(block_start, block_end+1)
 
     -- multiline blocks can specify a !! command on the second line
     -- e.g. # !! < input | grep result | wc -l
     local bangbang = "!!"
     if block_start ~= block_end then
-        local line = vim.api.nvim_buf_get_lines(buf.id, block_start+1, block_start+2, true)[1]
+        local line = buf:get_line(block_start+1)
         if line:match("^#.*!!") then bangbang = line:sub(2) end
     end
 
@@ -98,117 +116,86 @@ end
 
 function M.kill()
     -- actual implementation is set each time run is called
-    os.execute("echo old impl >> /tmp/out")
+end
+
+function counter(i)
+    return function()
+        local buf = i
+        i = i + 1
+        return buf
+    end
 end
 
 function M.run()
     local buf = buffer_data();
     exe_start, exe_end = find_run_block(buf)
+    -- print("run block: ["..str(exe_start)..", "..str(exe_end).."]")
     out_start, out_end = get_out_block(buf, exe_start, exe_end)
+    -- print("out block: ["..str(out_start)..", "..str(out_end).."]")
 
     -- clear out block
     if out_start ~= nil and out_end ~= nil then
-        vim.api.nvim_buf_set_lines(
-            buf.id, out_start, out_end+1, true, {}
-        )
+        buf:set_lines(out_start, out_end+1, {})
     end
 
     local cmd = create_cmd(buf, exe_start, exe_end)
 
-    local stdin = vim.loop.new_pipe()
-    local stdout = vim.loop.new_pipe()
-    local stderr = vim.loop.new_pipe()
+    local stdin = vim.loop.new_pipe(false)
+    local stdout = vim.loop.new_pipe(false)
+    local stderr = vim.loop.new_pipe(false)
 
-    local out_line = exe_end + 1
-    local handle, pid = vim.loop.spawn("bash", {
-        stdio = {stdin, stdout, stderr}
+    local handle, pid
+
+    handle, pid = vim.loop.spawn("bash", {
+        stdio = {stdin, stdout, stderr},
+        detached = true,
     }, function(code, signal) -- on exit
-        stderr:read_stop()
-        stderr:shutdown()
-        stdout:read_stop()
-        stdout:shutdown()
+        handle:close()
+        M.kill = function() end
     end)
 
     M.kill = function(signum)
-        os.execute("echo ran kill >> /tmp/out")
-        -- vim.loop.kill(pid, signum)
-        handle:kill(signum)
+        os.execute("kill -2 -"..pid)
         handle:close()
-        stderr:read_stop()
-        stderr:shutdown()
-        stdout:read_stop()
-        stdout:shutdown()
+        M.kill = function() end
     end
 
-    vim.api.nvim_buf_set_lines(
-         buf.id, out_line, out_line, true, {OUT_BLOCK_START}
-    )
+    local next_line = counter(exe_end + 1)
+    buf:append_line(next_line(), OUT_BLOCK_START)
+
+    local on_update = function(data)
+        vim.schedule(function()
+            for line in data:gmatch("([^\n]*)\n") do
+                buf:append_line(next_line(), line)
+            end
+            vim.api.nvim_command('redraw')
+        end)
+    end
 
     stdout:read_start(function(err, data)
         assert(not err, err)
         if data then
+            on_update(data)
+        else
             vim.schedule(function()
-                for line in data:gmatch("([^\n]*)\n") do
-                    out_line = out_line + 1
-                    vim.api.nvim_buf_set_lines(
-                        buf.id, out_line, out_line, true, {line}
-                    )
-                end
-                vim.api.nvim_command('redraw')
+                buf:append_line(next_line(), OUT_BLOCK_END)
             end)
-        else 
-            vim.schedule(function()
-                out_line = out_line + 1
-                vim.api.nvim_buf_set_lines(
-                     buf.id, out_line, out_line, true, {OUT_BLOCK_END}
-                )
-            end)
+            stdout:close()
         end
     end)
 
     stderr:read_start(function(err, data)
         assert(not err, err)
         if data then
-            vim.schedule(function()
-                for line in data:gmatch("([^\n]*)\n") do
-                    out_line = out_line + 1
-                    vim.api.nvim_buf_set_lines(
-                        buf.id, out_line, out_line, true, {line}
-                    )
-                end
-                vim.api.nvim_command('redraw')
-            end)
+            on_update(data)
+        else
+            stderr:close()
         end
     end)
 
-    stdin:write(cmd, function()
-        os.execute("echo 'write is done' >> /tmp/out")
-    end)
+    stdin:write(cmd)
     stdin:shutdown()
 
-    -- print("run block: ["..str(exe_start)..", "..str(exe_end).."]")
-    -- print("out block: ["..str(out_start)..", "..str(out_end).."]")
 end
 
-
-function M.test()
-    local signal = vim.loop.new_signal()
-    vim.loop.signal_start_oneshot(signal, 17, function(num)
-        local pid = vim.loop.os_getpid()
-        os.execute("echo finished >>/tmp/out")
-    end)
-
-    vim.loop.read_start(stdout, function(err, data)
-      assert(not err, err)
-      if data then
-        os.execute("printf '"..data.."' >> /tmp/out")
-      else
-        os.execute("echo done >> /tmp/out")
-      end
-    end)
-
-    print("stdin")
-    print(stdin)
-    vim.loop.write(stdin, "echo hello\n")
-end
 return M
--- plugin/justtxt.vim
+++ plugin/justtxt.vim
@@ -5,4 +5,3 @@ let g:loaded_justtxt = 1
 
 command! -nargs=0 JustTxtRun lua require("justtxt").run()
 command! -nargs=0 JustTxtKill lua require("justtxt").kill(9)
-command! -nargs=0 Test lua require("justtxt").test()
